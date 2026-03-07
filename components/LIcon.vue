71f6c4d6a8bf5cbc351b45ed1858e970db2952cb
idisgh <idisgh@gmail.com>
2026-03-07 23:42:29 +0900

    fix: only pass fill/stroke when fill prop is set, don't override defaults

--- components/LIcon.vue
+++ components/LIcon.vue
@@ -57,5 +57,5 @@ const iconComponent = computed(() => iconMap[props.name] || null)
 </script>
 
 <template>
-  <component v-if="iconComponent" :is="iconComponent" v-bind="attrs" :fill="fill && fill !== 'none' ? fill : 'none'" :stroke="fill && fill !== 'none' ? 'none' : undefined" />
+  <component v-if="iconComponent" :is="iconComponent" v-bind="{ ...attrs, ...(fill ? { fill, stroke: 'none' } : {}) }" />
 </template>
